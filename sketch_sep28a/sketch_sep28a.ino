f9b4d70ec06a7d82f3d4debbad298f16b6554510
huihui <s810646@email.nlhs.tyc.edu.tw>
2020-09-28 16:06:52 +0800

    修改為斷線時才會警告

--- sketch_sep28a/sketch_sep28a.ino
+++ sketch_sep28a/sketch_sep28a.ino
@@ -2,9 +2,13 @@ void setup()
 {
 pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
 }
 void loop()
 {
+if(digitalRead(2)==HIGH) {
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 tone(7, 1000, 500);
@@ -13,4 +17,5 @@ digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
 tone(7, 800, 500);
 delay(500);
+}
 }
